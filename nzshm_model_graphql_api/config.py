d1f274b503f6e35eec61e138b1df30d2291af318
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-11-06 15:42:24 +1300

    add indexing to elasticsearch;

---
+++ nzshm_model_graphql_api/config.py
@@ -0,0 +1,21 @@
+"""
+This module exports comfiguration for the current system
+"""
+import os
+
+
+def boolean_env(environ_name, default='FALSE'):
+    return bool(os.getenv(environ_name, default).upper() in ["1", "Y", "YES", "TRUE"])
+
+
+IS_OFFLINE = boolean_env('SLS_OFFLINE')  # set by serverless-wsgi plugin
+REGION = os.getenv('REGION', 'ap-southeast-2')
+DEPLOYMENT_STAGE = os.getenv('DEPLOYMENT_STAGE', 'LOCAL').upper()
+STACK_NAME = os.getenv('STACK_NAME', 'nshm_hazard_graphql_api')
+LOGGING_CFG = os.getenv('LOGGING_CFG', './logging_aws.yaml')
+ENABLE_METRICS = bool(os.getenv('ENABLE_METRICS', '').upper() in ["1", "Y", "YES", "TRUE"])
+CW_METRICS_RESOLUTION = os.getenv('CW_METRICS_RESOLUTION', 60)  # 1 for high resolution or 60
+
+ES_HOST = 'https://search-nzshm22-toshi-api-es-test-ybx3zlp6hz2shrytj2ns4zx6bm.ap-southeast-2.es.amazonaws.com' # TOSHI_TEST
+# ES_HOST = "https://search-nzshm22-toshi-api-es-prod-cj4taqcgnefophpxzan55xeswa.ap-southeast-2.es.amazonaws.com" # TOSHI_PROD
+
---
+++ scripts/elastic_cli.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from elasticsearch_dsl import Document, Date, Integer, Keyword, Text, Index, connections
+
+import nzshm_model
+
+COMMON_INDEX = 'toshi_nzshm_model_graphql_api_index'
+models_index = Index(COMMON_INDEX)
+models_index.settings(
+    number_of_shards=1,
+    number_of_replicas=0
+)
+
+from nzshm_model_graphql_api.config import ES_HOST
+from elasticsearch import RequestsHttpConnection
+
+# Define a default Elasticsearch client
+connections.create_connection(hosts=ES_HOST)
+# connections.create_connection(hosts="http://localhost:9200",
+#     verify_certs=False,
+#     connection_class=RequestsHttpConnection
+# )
+
+class SeismicHazardModelDocument(Document):
+    version = Text(fields={'raw': Keyword()})
+    notes = Text(fields={'raw': Keyword()})
+
+    clazz_name = Text(fields={'raw': Keyword()})
+
+    class Index:
+        name = COMMON_INDEX
+
+    def save(self, ** kwargs):
+        return super(SeismicHazardModelDocument, self).save(** kwargs)
+
+
+# create the mappings in elasticsearch
+SeismicHazardModelDocument.init()
+
+# create and save and article
+print(dir(nzshm_model.versions))
+for key, model in nzshm_model.versions.items():
+    # model = nzshm_model.get_version(key)
+    doc = SeismicHazardModelDocument(
+        _id = f'SeismicHazardModel_{model.version}',
+        # node_id == base64.uuencode(_id)
+        clazz_name='SeismicHazardModel',
+        version=model.version,
+        notes=model.title
+    )
+    doc.save()
+
+# article = Article.get(id=42)
+# print(article.is_published())
+
+# Display cluster health
+print(connections.get_connection().cluster.health())
